324de5b9d8ea1f460bf337f9f0e562797539774b
gotsejeong <dbdnpxm@naver.com>
2017-07-23 22:13:05 +0900

    pasing_master

--- app/src/main/java/com/example/miroku/weatherinfo/MainActivity.java
+++ app/src/main/java/com/example/miroku/weatherinfo/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -24,129 +26,69 @@ public class MainActivity extends AppCompatActivity {
     public String protocol = "GET";
     String ip;
     TextView textview;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Address = "http://www.findip.kr/";
-
         StringBuffer buffer = new StringBuffer();
         StringBuilder builder = new StringBuilder();
         textview = (TextView) findViewById(R.id.textView);
-        final Handler handler = new Handler() {
-            public void handleMessage(Message msg) {
-                textview.setText(ip);
-            }
-        };
+        button = (Button) findViewById(R.id.button);
 
 
-        new Thread() {
-        public void run(){
-            try{
-                url = new URL(Address);
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod(protocol);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-                String line;
-                while((line = br.readLine())!=null){
-                    if(line.startsWith("<h1 id=\"title\">")) {
-                        ip = line.replace("<h1 id=\"title\">","").replace("(My IP Address)","").replace("<br/>","");
-                        Log.e("ip=",ip);
+                textview.setText("IP주소를 불러오고 있습니다....");
+
+                final Handler handler = new Handler() {
+                    public void handleMessage(Message msg) {
+                        textview.setText(ip);
                     }
-                }
-                Message msg = handler.obtainMessage();
-                handler.sendMessage(msg);
-            } catch(
-            UnsupportedEncodingException e) {
-                e.printStackTrace();
-            } catch(
-            ProtocolException e) {
-                e.printStackTrace();
-            } catch(
-            MalformedURLException e) {
-                e.printStackTrace();
-            } catch(
-            IOException e) {
-                e.printStackTrace();
-                }
+                };//헨들러 ,쓰레드 속에 UI변경할때는 헨들러를 써야함
+
+                new Thread() {
+                    public void run(){
+                        try{
+                            url = new URL(Address);
+                            conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod(protocol);
+
+                            br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+                            String line;
+                            while((line = br.readLine())!=null){
+                                if(line.startsWith("<h1 id=\"title\">")) {
+                                    ip = line.replace("<h1 id=\"title\">","").replace("(My IP Address)","").replace("<br/>","");
+                                    Log.e("ip=",ip);
+                                }
+                            }
+                            //헨들러
+                            Message msg = handler.obtainMessage();
+                            handler.sendMessage(msg);
+                            //------
+                        } catch(
+                                UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        } catch(
+                                ProtocolException e) {
+                            e.printStackTrace();
+                        } catch(
+                                MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch(
+                                IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
             }
-    }.start();
-
-
-//        TextView textView = (TextView)findViewById(R.id.textView);
-//
-//        WeatherConnection weatherConnection = new WeatherConnection();
-//
-//        AsyncTask<String, String, String> result = weatherConnection.execute("","");
-//
-//        System.out.println("RESULT");
-//
-//        try{
-//            String msg = result.get();
-//            System.out.println(msg);
-//
-//            textView.setText(msg.toString());
-//
-//        }catch (Exception e){
-//
-//        }
-//    }
-//
-//    // 네트워크 작업은 AsyncTask 를 사용해야 한다
-//    public class WeatherConnection extends AsyncTask<String, String, String>{
-//
-//        // 백그라운드에서 작업하게 한다
-//        @Override
-//        protected String doInBackground(String... params) {
-//
-//
-//            // Jsoup을 이용한 날씨데이터 Pasing하기.
-//            try{
-//
-//                String path = "http://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09650510";
-//
-//                Document document = Jsoup.connect(path).get();
-//
-//                Elements elements = document.select("em");
-//
-//                System.out.println(elements);
-//
-//                Element targetElement = elements.get(2);
-//
-//                String text = targetElement.text();
-//
-//                System.out.println(text);
-//
-//                return text;
-//
-//
-//                /*
-//                // URL접속하여 HTML 가져오기.
-//                URL url = new URL("http://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09650510");
-//
-//                System.out.println("Call target URL try....");
-//
-//                InputStream inputStream = url.openStream();
-//                Scanner scanner = new Scanner(inputStream, "UTF-8");
-//
-//                 while (true){
-//                    try{
-//                        System.out.println(scanner.nextLine());
-//                    }catch (Exception e){
-//                        e.printStackTrace();
-//                        break;
-//                    }
-//                }
-//                */
-//            }catch (Exception e){
-//                e.printStackTrace();
-//            }
-//            return null;
-//        }
-//    }
+        });
     }
+
+
 }
